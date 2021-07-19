58059b3cdc1496a82766ad8d998d02585a39d85a
Nilberto Junor <nilbertojr28@gmail.com>
2021-07-18 23:55:44 -0300

    Adicionando script ao projeto

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+/**
+ * Gulpfile
+ * @author Nil Junior <nil.junior@agencianidi.com.br>
+ */
+ 'use strict';
+
+ const gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifyCSS = require('gulp-clean-css');
+
+
+const FOLDER = "./src/";
+const CSSPATH = FOLDER + '/css/';
+const SASSPATH = FOLDER + 'assets/sass/';
+const JSPATH = FOLDER + '/js/';
+const IMGPATH = FOLDER + 'assets/images/';
+
+gulp.task('sass', function() {
+
+    return gulp.src(SASSPATH + 'general_styles.scss')
+         .pipe(sass({
+             errLogToConsole: true
+         }))
+         .on('error', sass.logError)
+         .pipe(concat('style.css'))
+         .pipe(autoprefixer())
+         .pipe(minifyCSS())
+         .pipe(gulp.dest(CSSPATH));
+ });
+
+ gulp.task('watch', function()  {
+    gulp.watch(SASSPATH + 'sass/**/*', gulp.parallel('sass'));
+
+});
